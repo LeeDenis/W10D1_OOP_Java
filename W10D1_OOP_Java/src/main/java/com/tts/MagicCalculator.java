7747945d94e91f6d8b5930c78fa9abd0578f0499
LeeDenis <79125449+LeeDenis@users.noreply.github.com>
2021-04-20 08:58:48 -0400

    Add files via upload

---
+++ W10D1_OOP_Java/src/main/java/com/tts/MagicCalculator.java
@@ -0,0 +1,37 @@
+package com.tts;
+
+public class MagicCalculator {
+
+    public static void sqrt(double num1) {
+        double root = Math.sqrt(num1);
+        System.out.println("The square root of " + num1 + " is " + root + ".");
+    }
+
+    public static void sin(double num1) {
+        double sin = Math.sin(num1);
+        System.out.println("The sine of " + num1 + " is " + sin + ".");
+    }
+
+    public static void cos(double num1) {
+        double cos = Math.cos(num1);
+        System.out.println("The cosine of " + num1 + " is " + cos + ".");
+    }
+
+    public static void tan(double num1) {
+        double tan = Math.tan(num1);
+        System.out.println("The tangent of " + num1 + " is " + tan + ".");
+    }
+
+    public static void factorial(double num1) {
+        double factorial = 1;
+        if (num1 == 0 || num1 == 1) {
+            factorial = 1;
+            System.out.println("The factorial of " + num1 + " is " + factorial + ".");
+        } else {
+            for (var i = num1; i > 1; i--) {
+                factorial = factorial * i;
+            }
+            System.out.println("The factorial of " + num1 + " is " + factorial + ".");
+        }
+    }
+}
